f1e12081568d6b3637813e356dd32e3bcf65fe72
Roshni Barodia <roshnibarodia@Roshnis-MacBook-Pro.local>
2020-05-15 17:22:37 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Fantasy-Trading
+
+Setup Instructions
+  1. Install postgresql 11
+  2. Install node.js
+  3. Create user (name: fantasytrading) in postgresql with permissions "can login"
+  4. Set password for this user
+  5. Create database (name: fantasytrading) owned by user fantasytrading
+  6. Create file called .env and copy and paste the contents from .env_template into this file --> fill in DB password field
+  7. npm install
+  8. npm start
+  9. server on localhost:5000
--- server.js
+++ server.js
@@ -11,33 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use('/api', usersRoute);
 
-// const getUsers = (request, response) => {
-//   pool.query("SELECT * FROM users", (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(200).json(results.rows)
-//   })
-// }
-
-// const addBook = (request, response) => {
-//   const { author, title } = request.body
-//
-//   pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(201).json({ status: 'success', message: 'Book added.' })
-//   })
-// }
-
-// app
-  //.route('/users')
-  // GET endpoint
-  // .get(getUsers)
-  // POST endpoint
-  //.post(addBook)
-
 // Start server
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server listening`)
