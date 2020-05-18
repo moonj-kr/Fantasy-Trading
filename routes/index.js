7894ff9888ef8a254d07f7612b316f9215391389
Roshni Barodia <roshnibarodia@Roshnis-MacBook-Pro.local>
2020-05-18 11:13:13 -0400

    setting up swagger

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const usersController = require('../controllers').usersController;
+var swaggerUi = require('swagger-ui-express')
+var swaggerJSDoc = require('swagger-jsdoc');
+const swaggerDefinition = {
+  info: {
+    title: 'Fantasy Trading Swagger API',
+    version: '1.0.0',
+    description: 'Endpoints exposed from the Fantasy Trading backend'
+  },
+  host: 'localhost:5000',
+  basepath: '/'
+}
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js']
+}
+const swaggerSpec = swaggerJSDoc(options);
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Fantasy Trading API!',
   }));
-
+  app.get('/swagger.json', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   app.get('/api/users', usersController.list);
 
 };
