5caa272478b27d9d0c0724e6f0f1e6b79d4cfc7e
Luis Gutierrez <luisgutierrezgalaviz@gmail.com>
2020-05-19 13:19:59 -0400

    PLURAL

--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
     User.hasMany(models.Portfolio, {
       foreignKey: 'userID',
-      as: 'portfolio',
+      as: 'portfolios',
     });
   };
   return User;
