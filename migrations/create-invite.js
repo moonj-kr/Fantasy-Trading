2eea66eaad6960c1ca1b56ae149aebcd0a8fef93
Roshni Barodia <roshnibarodia@Roshnis-MacBook-Pro.local>
2020-05-19 13:04:34 -0400

    creating invite table and model

---
+++ migrations/create-invite.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Invites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      invitationKey: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.ENUM(['No Account', 'Pending', 'Accepted'])
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Invites');
+  }
+};
