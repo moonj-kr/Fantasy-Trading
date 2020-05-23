d08902e5c39d175e7bdede0881d17e42f418cb33
Roshni Barodia <roshnibarodia@Roshnis-MacBook-Pro.local>
2020-05-23 09:17:33 -0400

    updating invitation process

--- controllers/leaguesController.js
+++ controllers/leaguesController.js
@@ -4,8 +4,21 @@ const Portfolio = require('../models').Portfolio;
 const Invite = require('../models').Invite;
 var schedule = require('node-schedule');
 const nodemailer = require("nodemailer");
+var fs = require('fs');
+var handlebars = require('handlebars');
 
 
+var readHTMLFile = function(path, callback) {
+    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
 function generateInviteKey(){
   const length = 10;
   return Math.random().toString(20).substr(2, length);
@@ -16,6 +29,7 @@ function scheduleJobs(startDateString, endDateString){
   var leagueRankingJob = schedule.scheduleJob({ start: startDate, end: endDate, rule: '*****7' }, function(){
     console.log('This is where we would call the league ranking api every week!');
   });
+  console.log(leagueRankingJob);
   var globalRankingJob = schedule.scheduleJob(endDate, function(){
     console.log('This is where we would call the global ranking api at the end of the league!');
   });
@@ -23,32 +37,58 @@ function scheduleJobs(startDateString, endDateString){
 function sendEmail(email, firstName, leagueName){
   nodemailer.createTestAccount((err, account) => {
     let transporter = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.mailtrap.io",
+      port: 587,
+      secure: false, // true for 465, false for other ports
       auth: {
-        user: "fantasytrading2020",
-        pass: "ritswen2020"
-      }
-    });
-    let mailOptions = {
-      from: '"Fantasy Trading" <fantasytrading2020@gmail.com>', // sender address
-      to: email, // list of receivers
-      subject: "You've been invited to join " + leagueName, // Subject line
-      text: firstName + " has invited you to join their league " + leagueName + "! \n Compete with your friends and show off your stock trading skills! \n Please click the link below to accept their invite" , // plain text body
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if(error){
-        return console.log(error);
-      }
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        user: '9611e4ce3d32bc', // generated ethereal user
+        pass: 'd70743dcdd9f9a', // generated ethereal password
+      },
 
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      // service: "Gmail",
+      // auth: {
+      //   user: "fantasytrading2020",
+      //   pass: "ritswen2020"
+      // }
+    });
+    readHTMLFile(__dirname + '/email.html', function(err, html) {
+      var template = handlebars.compile(html);
+      var replacements = {
+           firstName: firstName,
+           leagueName: leagueName
+      };
+      var htmlToSend = template(replacements);
+      let mailOptions = {
+        from: '"Fantasy Trading" <fantasytrading2020@gmail.com>', // sender address
+        to: email, // list of receivers
+        subject: "You've been invited to join " + leagueName, // Subject line
+        text: firstName + " has invited you to join their league " + leagueName + "! \nCompete with your friends and show off your stock trading skills! \nPlease click the link below to accept their invite" , // plain text body
+        html: htmlToSend
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+          return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      });
     });
   });
 }
 
 module.exports = {
+  getNumberOfPartcipants(req, res){
+    const leagueName = req.params.leagueName;
+    console.log(leagueName);
+    League.findOne({where: {leagueName: leagueName}}).then(league => {
+      Portfolio.count({where: {leagueID: league.id}}).then(count => {
+        res.status(200).send(count);
+      }).catch(error => {console.error(error)});
+    }).catch(error => {console.error(error)});
+  },
   list(req, res){
     return League.findAll().then((leagues) => {res.status(200).send(leagues)}).catch(error => {res.status(400).send(error)})
   },
@@ -90,18 +130,22 @@ module.exports = {
               Invite.findOrCreate({where: {email: address, invitationKey: invitationKey}}).then((invite,created) => {
                 User.findOne({where: {email: address}}).then(user => {
                   if(user){
-                    invite.status = 'Pending';
+                    Invite.update({status: 'Pending'}, {where: {email: address, invitationKey: invitationKey}}).then((affectedCount, affectRows) => {
+                      console.log('invite status set to pending')
+                    }).catch(error => {console.error(error)});
                   }
                   else{
-                    invite.status = 'No Account';
+                    Invite.update({status: 'No Account'}, {where: {email: address, invitationKey: invitationKey}}).then((affectedCount, affectRows) => {
+                      console.log('invite status set to no account');
+                    }).catch(error => {console.error(error)});
                   }
                 }).catch(error => {console.error(error)});
               }).catch(error => {console.error(error)});
             });
-            res.status(200).send(portfolio)
           }
+          res.status(200).send(invitationKey);
         }).catch(error => {console.error(error)});
       }).catch(error => {console.error(error)});
     }).catch(error => {console.error(error)});
-  }
+  },
 };
--- routes/leaguesRoute.js
+++ routes/leaguesRoute.js
@@ -1,5 +1,6 @@
 const leaguesController = require('../controllers').leaguesController;
 module.exports = (app) => {
+  app.get('/api/leagues/participants/:leagueName', leaguesController.getNumberOfPartcipants);
   app.post('/api/leagues/create', leaguesController.create);
   app.post('/api/leagues/sendInvite', leaguesController.sendInvite);
   app.get('/api/leagues/list', leaguesController.list);
