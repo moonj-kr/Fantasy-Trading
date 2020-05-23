4ccce56b47bfc1f417e8e65ea933aeac8fc19f72
Christopher Savan <cxs7700@rit.edu>
2020-05-23 14:44:19 -0400

    starter code for calculateing portfolio value

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -6,36 +6,51 @@ module.exports = {
   
   getAllStocks(req,res) {
     return Stock
-    .findAll()
-    .then(stocks => res.status(200).send(stocks))
-    .catch(error => res.status(400).send('Get all stocks error: ', error))
+      .findAll()
+      .then(stocks => res.status(200).send(stocks))
+      .catch(error => res.status(400).send('Get all stocks error: ', error))
   },
   
   getStockData(req,res) {
     const symbol = req.params.symbol.toUpperCase()
     return Stock
-    .findOne({where: {symbol: symbol}})
-    .then(stock => {
-      if (stock.isUpdated === false) {
-        // POST StockDataAPI (ProcessStockData) to update stock price based on previous close
-        // here 
-        
-        const date = new Date(Date.now())
-        stock.update({
-          isUpdated: true,
-          updatedAt: date
-        })
-        .then(() => res.status(200).send(`Stock ${symbol} has been updated.`))
-      } else {
-        res.status(200).send(stock)
-      }
-    })
-    .catch(error => res.status(400).send(error));
+      .findOne({where: {symbol: symbol}})
+      .then(stock => {
+        if (stock.isUpdated === false) {
+          // HERE: POST StockDataAPI (ProcessStockData) to update stock price based on previous close
+          // Update stock price using stock.update()
+          
+          const date = new Date(Date.now())
+          stock.update({
+            isUpdated: true,
+            updatedAt: date
+          })
+          .then(() => res.status(200).send(`Stock ${symbol} has been updated.`))
+        } else {
+          res.status(200).send(stock)
+        }
+      })
+      .catch(error => res.status(400).send(error));
   },
+  
   getAllPortfolios(req,res) {
     return Portfolio
-    .findAll()
-    .then(portolio => res.status(200).send(portolio))
-    .catch(error => res.status(400).send(error))
+      .findAll()
+      .then(portolios => res.status(200).send(portolios))
+      .catch(error => res.status(400).send(error))
+  },
+  
+  // confused here
+  calculatePortfolioValue(req,res) {
+    let portfolioValue;
+    let shares
+    const leagueId = req.params.id;
+    return Portfolio
+      .findOne({where: {leagueID: leagueId}})
+      .then(portfolio => {
+        
+      })
+      .catch(error => res.status(400).send(error));
   }
+  
 };
--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -3,5 +3,7 @@ const transactionController = require('../controllers').transactionController;
 module.exports = (app) => {
   app.get('/api/stocks', transactionController.getAllStocks);
   app.get('/api/stocks/:symbol', transactionController.getStockData);
-  app.post('api/portfolios/:id', transactionController.getAllPortfolios);
+  app.get('api/portfolios', transactionController.getAllPortfolios);
+  app.post('api/portfolios/:id', transactionController.calculatePortfolioValue);
+  // 
 };
