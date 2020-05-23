7fed29c4dbda9d0c56d0997fe5f9e5d95b4b7490
Christopher Savan <cxs7700@rit.edu>
2020-05-23 14:44:52 -0400

    committing to checkout

--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -4,6 +4,6 @@ module.exports = (app) => {
   app.get('/api/stocks', transactionController.getAllStocks);
   app.get('/api/stocks/:symbol', transactionController.getStockData);
   app.get('api/portfolios', transactionController.getAllPortfolios);
-  app.post('api/portfolios/:id', transactionController.calculatePortfolioValue);
-  // 
+  app.post('api/portfolios/:id', transactionController.calculatePortfolioValue); // id is leagueID
+  // Need POST stock data API
 };
