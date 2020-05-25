805b04328dbf13fef55b39ee6de814133674d4ba
Luis Gutierrez <luisgutierrezgalaviz@gmail.com>
2020-05-25 18:17:40 -0400

    User routes and post register handler function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,45 @@
-const User = require('../models').User;
-
+const models = require('../models');
+const env = process.env.NODE_ENV || 'development';
+const config = require(`${__dirname}/../config/config.json`)[env];
+const bcrypt = require("crypt");
 
 module.exports = {
-  create(req, res) {
-    return User
-      .create({
-        content: req.body.content,
-        userId: req.params.userId,
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
   list(req, res) {
-    return User
+    return models.User
       .findAll()
       .then((users) => res.status(200).send(users))
       .catch((error) => res.status(400).send(error));
   },
+  get_register(req, res){
+    res.render('registration');
+  },
+  post_register(req, res) {
+    let salt = config.salt;
+    bcrypt.hash(salt + req.body.password, 10, function(err, hash){
+      models.User.create({
+        username: req.body.username,
+        password: hash,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        points: req.body.points,
+        sessionID: req.params.userId,
+      }).then(user => res.status(201).send(user))
+        .catch(error => res.status(400).send(error));
+    });
+  },
+  get_login(req, res){
+    res.render('login');
+  },
+  post_login(req, res){
+    //const sessionID =
+  },
+  logout(req, res){
+
+  },
+  leagues(req, res){
+
+  }
   // login(req, res){
   //   return User
   //     .findOne({where: {username: req.body.username, password: req.body.password}})
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,6 +1,12 @@
 const usersController = require('../controllers').usersController;
-
 module.exports = (app) => {
   app.get('/api/users', usersController.list);
-  //app.post('/api/login', usersController.login)
+
+  app.get('/api/users/register', usersController.get_register);
+  app.get('/api/users/login', usersController.get_login);
+
+  app.post('/api/users/register', usersController.post_register);
+  app.post('/api/users/login', usersController.post_login);
+  app.get('/api/users/logout', usersController.logout);
+  app.get('/api/users/leagues', usersController.leagues);
 }
