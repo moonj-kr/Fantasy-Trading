afd738f3df6da57ac145d18a882dd9685f832584
Luis Gutierrez <luisgutierrezgalaviz@gmail.com>
2020-05-25 18:15:55 -0400

    Added pug view engine to test user registration. DELETE before merging to master.

--- server.js
+++ server.js
@@ -3,8 +3,14 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
