cdf2b50471dfd53c60730f52c5be4abe88c09482
Christopher Savan <cxs7700@rit.edu>
2020-05-27 13:22:29 -0400

    make transaction route and controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,6 +1,7 @@
 const Stock = require('../models').Stock;
-const Portfolio = require('../models').Portfolio
-const League = require('../models').League
+const Portfolio = require('../models').Portfolio;
+const League = require('../models').League;
+const Tranasaction = require('../models').Transaction;
 
 module.exports = {
   
@@ -33,24 +34,31 @@ module.exports = {
       .catch(error => res.status(400).send(error));
   },
   
-  getAllPortfolios(req,res) {
-    return Portfolio
-      .findAll()
-      .then(portolios => res.status(200).send(portolios))
-      .catch(error => res.status(400).send(error))
-  },
-  
-  // confused here
-  calculatePortfolioValue(req,res) {
-    let portfolioValue;
-    let shares
-    const leagueId = req.params.id;
-    return Portfolio
-      .findOne({where: {leagueID: leagueId}})
-      .then(portfolio => {
-        
-      })
-      .catch(error => res.status(400).send(error));
+  makeTransaction(req,res) {
+    const leagueId = req.body.leagueId; // may need to check where this leagueID is coming from in the frontend.
+    let transactionValue;
+    return Transaction
+      // Create a new transaction in the database using information from the request body.
+      .create({
+        volume: req.body.volume,
+        price: req.body.price,
+        type: req.body.type,
+        stockID: req.body.symbol
+      }).then(transaction => {
+        // Get portfolio by leagueID, then use the transaction to update the portfolio value.
+        Portfolio.findOne({where: {leagueID: leagueId}}).then(portfolio => {
+          transactionValue = transaction.type === 'buy' ? transaction.volume * transaction.price : transaction.volume * transaction.price * -1;
+          portvolio.value + transactionValue < 0 ? portfolio.value = 0 : portfolio.value += transactionValue;
+          console.log("Transaction value: " + transactionValue);
+          console.log("Portfolio value: " + portfolio.value);
+        }).catch(error => {
+          console.log(error)
+          res.status(400).send(error)
+        })
+        res.status(200).send(transaction)
+      }).catch(error => {
+        console.log(error)
+        res.status(400).send(error)
+      });
   }
-  
 };
--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -3,7 +3,5 @@ const transactionController = require('../controllers').transactionController;
 module.exports = (app) => {
   app.get('/api/stocks', transactionController.getAllStocks);
   app.get('/api/stocks/:symbol', transactionController.getStockData);
-  app.get('api/portfolios', transactionController.getAllPortfolios);
-  app.post('api/portfolios/:id', transactionController.calculatePortfolioValue); // id is leagueID
-  // Need POST stock data API
+  app.post('api/portfolios/makeTransaction', transactionController.makeTransaction); 
 };
