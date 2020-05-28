4a25dd54493c5be2cc387d70dd402547330181cb
Luis Gutierrez <luisgutierrezgalaviz@gmail.com>
2020-05-28 10:00:30 -0400

    POST login and GET leagues user apis
    Users can now login using their username or email.
    GET leagues fetches all leagues user is currently participating in.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,20 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const bcrypt = require("bcrypt");
 const {QueryTypes} = require('sequelize');
+const Sequelize = require('sequelize');
+var leagues = [];
+
+//function pushElement(element) {
+//  return leagues.push(element);
+//}
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
+}
 
 module.exports = {
   list(req, res) {
@@ -52,48 +66,50 @@ module.exports = {
   get_login(req, res){
     res.render('login');
   },
-  post_login(req, res){
+  async post_login(req, res){
     let salt = config.salt;
-    models.User.findOne({where: {email: req.body.email}}).then(function(user) {
-      //if email is not registered redirect to login page
-      if (!user){res.redirect('/api/users/login');}
-      else{
-        bcrypt.compare(salt + req.body.password, user.password, function(err, result){
-          if (result == true){
-            console.log(req.sessionID);
-            user.sessionID = req.sessionID;
-            user.save();
-            //invitationKey passed in if login triggered from invitation email
-            if(req.body.invitationKey){
-              //Update invitation status
-              models.Invite.findOne({where: {email: user.email, invitationKey: req.body.invitationKey}}).then(invite => {
-                invite.status = 'Accepted';
-                invite.save();
+    let user;
+    if(req.body.username){
+      user = await models.User.findOne({where: {username: req.body.username}});
+    }
+    else if (req.body.email) {
+      user = await models.User.findOne({where: {email: req.body.email}});
+    }
+    //if email is not registered redirect to login page
+    if (!user){res.redirect('/api/users/login');}
+    else{
+      bcrypt.compare(salt + req.body.password, user.password, function(err, result){
+        if (result == true){
+          console.log(req.sessionID);
+          user.sessionID = req.sessionID;
+          user.save();
+          //invitationKey passed in if login triggered from invitation email
+          if(req.body.invitationKey){
+            //Update invitation status
+            models.Invite.findOne({where: {email: user.email, invitationKey: req.body.invitationKey}}).then(invite => {
+              invite.status = 'Accepted';
+              invite.save();
+            }).catch(error => console.error(error));
+            //Create new portfolio
+            models.League.findOne({where: {invitationKey: req.body.invitationKey}}).then(league => {
+              models.Portfolio.create({
+                value: league.investmentFunds,
+                host: false,
+                ranking: null
+              }).then(portfolio => {
+                portfolio.setLeague(league);
+                portfolio.setUser(user);
               }).catch(error => console.error(error));
-              //Create new portfolio
-              models.League.findOne({where: {invitationKey: req.body.invitationKey}}).then(league => {
-                models.Portfolio.create({
-                  value: league.investmentFunds,
-                  host: false,
-                  ranking: null
-                }).then(portfolio => {
-                  portfolio.setLeague(league);
-                  portfolio.setUser(user);
-                }).catch(error => console.error(error));
-              }).catch(error => console.error(error));
-            }
-            res.status(200).send(user);
+            }).catch(error => console.error(error));
           }
-          else{
-            res.status(403).send('Incorrect password');
-            //res.redirect('/api/users/login');
-          }
-        });
-      }
-    }).catch(error => {
-      console.error(error);
-      res.status(400).send(error);
-    });
+          res.status(200).send(user);
+        }
+        else{
+          res.status(403).send('Incorrect password');
+          //res.redirect('/api/users/login');
+        }
+      });
+    }
   },
   logout(req, res){
     models.User.findOne({where: {sessionID: req.sessionID}}).then(user => {
@@ -106,21 +122,13 @@ module.exports = {
     });
   },
   leagues(req, res){
-    models.User.findOne({where: {sessionID: req.sessionID}}).then(user => {
-      models.Portfolio.findAll({where: {userID: user.id}}).then(portfolios => {
-        let leagues = [];
-        portfolios.forEach(portfolio => {
-          League.findOne({where: {id: portfolio.leagueID}).then(league =>{leagues.push(league)})
-        )});
+    sequelize.query(`SELECT "name" FROM "Leagues" l INNER JOIN "Portfolios" p ON "l"."id" =
+        "p"."leagueID" INNER JOIN "Users" u ON "p"."userID" = "u"."id" WHERE "u"."sessionID" =
+        '${req.sessionID}'`, { type: QueryTypes.SELECT }).then(leagues => {
+        res.status(200).send(leagues);
       }).catch(error => {
         console.log(error);
         res.status(400).send(error);
       });
-      console.log(error);
-      res.status(200).send(leagues)
-    }).catch(error => {
-      console.log(error);
-      res.status(400).send(error);
-    });
   }
 };
