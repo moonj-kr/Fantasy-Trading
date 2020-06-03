ce1996040a546097dcae46e7b1ea5aee3e701663
moonj-kr <moonj.kr@gmail.com>
2020-06-03 19:04:41 +0900

    starting new

--- routes/stockProcessRoute.js
+++ routes/stockProcessRoute.js
@@ -4,7 +4,8 @@ module.exports = (app) => {
 	app.get('/api/stocks/resetUpdates', stockProcessController.resetUpdates);
 	app.get('/api/stocks/isStockUpdated/:symbol', stockProcessController.isStockUpdated);
 	app.get('/api/stocks/updatePrice/:symbol', stockProcessController.updatePrice);
-	app.get('/api/stocks/getStockPrice', stockProcessController.getStockPrice);
+	app.get('/api/stocks/getStockPrice/:symbol', stockProcessController.getStockPrice);
 //	app.get('/api/stocks/addStock', stockProcessController.addStock);
-//	app.get('/api/stocks/deleteStock', stockProcessController.deleteStock);
+	app.get('/api/stocks/deleteStock/:symbol', stockProcessController.deleteStock);
+	app.get('/api/stocks/doesStockExist/:symbol', stockProcessController.doesStockExist);
 }
