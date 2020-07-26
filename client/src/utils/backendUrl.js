38d6f1e7463fec50d151e00751822a1c0a333d6c
Roshni Barodia <rxb9639@g.rit.edu>
2020-07-25 23:47:09 -0400

    implementing stock search on league page

--- client/src/utils/backendUrl.js
+++ client/src/utils/backendUrl.js
@@ -1,9 +1,13 @@
 const env = process.env.NODE_ENV || 'development';
 let backend_url;
+let alpha_api_key = '5EDZ5BZ8UBWSWBFJ';
+let alpha_search_endpoint = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH';
 if(env === 'development'){
   backend_url = 'http://localhost:5000/api';
 }
 
 module.exports = {
-  backend_url
+  backend_url,
+  alpha_api_key,
+  alpha_search_endpoint
 }
